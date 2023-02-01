20af31f35f39efb07dcb0e4bc3016b996c8734eb
marcelabus <guidoymarcela@hotmail.com>
2023-02-01 15:32:04 -0300

    fixed header and footer css

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,15 @@ import '../assets/css/App.css';
 import '../assets/css/colors.css';
 import Header from './Header';
 import Footer from './Footer';
+import ContentWrapper from './ContentWrapper';
 
 function App() {
   return (
     <div className="App">
 
         <Header />
+
+        <ContentWrapper />
         
         <Footer />
       
