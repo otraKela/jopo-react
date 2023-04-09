fcd9ecd7ef5f8d827a81b7eb27443fdde1c76374
marcelabus <guidoymarcela@hotmail.com>
2023-04-09 19:48:28 -0300

    eliminé elementos definidos y no usados

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,9 +1,9 @@
-export const GET_CATEGORIES_URL = 'http://localhost:3040/products/categories';
+export const GET_CATEGORIES_URL = `${process.env.REACT_APP_API_BASE_URL}products/categories`;
 
-export const GET_PRODUCTS_URL = 'http://localhost:3040/products';
+export const GET_PRODUCTS_URL = `${process.env.REACT_APP_API_BASE_URL}products`;
 
-export const POST_PRODUCTS_URL = 'http://localhost:3040/products';
+export const POST_PRODUCTS_URL = `${process.env.REACT_APP_API_BASE_URL}products`;
 
-export const VALIDATE_LOGIN_URL = 'http://localhost:3040/users/login';
+export const VALIDATE_LOGIN_URL = `${process.env.REACT_APP_API_BASE_URL}users/login`;
 
-export const POST_USERS_URL = 'http://localhost:3040/users';
+export const POST_USERS_URL = `${process.env.REACT_APP_API_BASE_URL}users`;
