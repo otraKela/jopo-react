691a610d6e0b290e8db8395a66060e8f5bd528f3
otraKela <bustamante.marce@gmail.com>
2023-06-14 12:58:26 -0300

    agregué console.logs para prueba

--- src/services/registerUser.js
+++ src/services/registerUser.js
@@ -1,7 +1,7 @@
 import { POST_USERS_URL, IMG_API_KEY } from './settings.js';
 
 async function registerUser (userData) {
-
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
   let userImg;
 
   const formData = new FormData();
@@ -17,6 +17,7 @@ async function registerUser (userData) {
   // stored
   if (!userData.img ) {
     userImg = null;
+console.log('userIg = null')
   } else {
     // In a development environment user images will be stored in a local folder
     // The image is appended to formData to be uploaded by multer
@@ -24,8 +25,9 @@ async function registerUser (userData) {
     if (process.env.NODE_ENV === 'development') {
       userImg = process.env.REACT_APP_IMG_LOCAL_FOLDER  + userData.img.name;
       formData.append ('image', userData.img);
-
+console.log('salió por development, userImg = ', userImg)
     } else {
+console.log('salió por no development')
       // In a production environment user images will be stored in an image server. 
       // After uploading the picture to the server the url is retrieved to be stored for future use
       const dateNow = Date.now();
@@ -46,7 +48,7 @@ async function registerUser (userData) {
         const response = await fetch( process.env.REACT_APP_IMG_API_URL, imgRequest );
         
         const result = await response.json();
-      
+console.log('result img', result)      
         if ( result.status && result.status === 200 ) {
           userImg = result.data.thumb.url;
         } else {
@@ -58,7 +60,7 @@ async function registerUser (userData) {
       }
     }
   }
-
+console.log('userImg', userImg)
     // Complete the user data
   formData.append ('img', userImg);  // user image url
 
@@ -72,7 +74,7 @@ async function registerUser (userData) {
     const response = await fetch(POST_USERS_URL, request);
 
     const result = await response.json();
-
+console.log('result data', result) 
     return result;
   }
   catch (error) {
