f978d92684a20ecaf69f7504588270ea0dff1023
otraKela <bustamante.marce@gmail.com>
2023-06-15 20:09:39 -0300

    pruebas cors

--- src/services/registerUser.js
+++ src/services/registerUser.js
@@ -65,25 +65,16 @@ async function registerUser (userData) {
 
   // Completing the registration process by adding the user to the DB
   try {
-console.log('entré al try de datos')
-console.log(formData.get('firstName'))
-console.log(formData.get('lastName'))
-console.log(formData.get('email'))
-console.log(formData.get('phone'))
-console.log(formData.get('password'))
-console.log(formData.get('img')
-)
+
     const request = {
       'headers': {
         'Referer': 'https://jopo-react.netlify.app/registration',
         'Content-Type': 'application/json'
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-        // 'Access-Control-Allow-Headers': 'multipart/form-data',
-        },
+         },
       'method': 'POST',
       'body': formData
      };
+console.log('request', request);
 console.log('linea 73')     
     const response = await fetch(POST_USERS_URL, request);
 console.log('linea 75')   
